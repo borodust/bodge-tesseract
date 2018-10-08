6da41e2a6ec3ad039c691d51866df246210d11a3
Pavel Korolev <dev@borodust.org>
2018-10-08 09:25:06 +0300

    Let claw define implicit package

--- claw.lisp
+++ claw.lisp
@@ -1,10 +1,5 @@
-(cl:defpackage :%tesseract
-  (:nicknames :%tess)
-  (:use))
-
 (claw:c-include "bodge_tesseract.h" bodge-tesseract
-  :in-package :%tess
-  :local-only t
+  :in-package (:%tesseract :%tess)
   :sysincludes (:tesseract-api-includes :tesseract-util-includes)
   :include-definitions ("Tess[A-Z]+\\w*")
   :rename-symbols (claw:by-removing-prefixes "Tess"))
